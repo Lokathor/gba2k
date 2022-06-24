ab8bf4d04250a7eaea401512c7a02744351614da
Lokathor <zefria@gmail.com>
2022-06-24 00:17:46 -0600

    forgot to add this last time.

--- src/bit_utils.rs
+++ src/bit_utils.rs
@@ -6,6 +6,86 @@
 /// * If the bit requested is out of range.
 #[inline]
 #[must_use]
+pub const fn u32_get_bit<const B: u32>(x: u32) -> bool {
+  assert!(B < 32);
+  let mask = 1 << B;
+  (x & mask) != 0
+}
+
+/// Replaces the `B` bit.
+///
+/// ## Panics
+/// * If the bit requested is out of range.
+#[inline]
+#[must_use]
+pub const fn u32_with_bit<const B: u32>(x: u32, val: bool) -> u32 {
+  assert!(B < 32);
+  let mask = 1 << B;
+  (x & !mask) | ((val as u32) << B)
+}
+
+/// Gets a `L` to `H` (inclusive) bit region of the value.
+///
+/// ## Panics
+/// * If `L` or `H` are out of range.
+/// * If `L` >= `H`
+#[inline]
+#[must_use]
+pub const fn u32_get_region<const L: u32, const H: u32>(x: u32) -> u32 {
+  assert!(L < 32);
+  assert!(H < 32);
+  assert!(L < H);
+  let mask = (((1_u64 << (H - L + 1)) - 1) << L) as u32;
+  assert!(mask.count_ones() == (H - L + 1));
+  x & mask
+}
+
+/// Replaces a `L` to `H` (inclusive) bit region of the value.
+///
+/// ## Panics
+/// * If `L` or `H` are out of range.
+/// * If `L` >= `H`
+#[inline]
+#[must_use]
+pub const fn u32_with_region<const L: u32, const H: u32>(
+  x: u32, val: u32,
+) -> u32 {
+  assert!(L < 32);
+  assert!(H < 32);
+  assert!(L < H);
+  let mask = (((1_u64 << (H - L + 1)) - 1) << L) as u32;
+  assert!(mask.count_ones() == (H - L + 1));
+  (x & !mask) | val
+}
+
+/// Like [`u32_get_region`] but the output is shifted down appropriately.
+///
+/// ## Panics
+/// * As `u32_get_region`
+#[inline]
+#[must_use]
+pub const fn u32_get_value<const L: u32, const H: u32>(x: u32) -> u32 {
+  u32_get_region::<L, H>(x) >> L
+}
+
+/// Like [`u32_with_region`] but the value is shifted up appropriately.
+///
+/// ## Panics
+/// * As `u32_with_region`
+#[inline]
+#[must_use]
+pub const fn u32_with_value<const L: u32, const H: u32>(
+  x: u32, val: u32,
+) -> u32 {
+  u32_with_region::<L, H>(x, val << L)
+}
+
+/// Gets the `B` bit.
+///
+/// ## Panics
+/// * If the bit requested is out of range.
+#[inline]
+#[must_use]
 pub const fn u16_get_bit<const B: u32>(x: u16) -> bool {
   assert!(B < 16);
   let mask = 1 << B;
