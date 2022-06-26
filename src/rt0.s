480c9eddd0d3dbfa19d379842ddcbe23a4d3d313
Lokathor <zefria@gmail.com>
2022-06-26 11:12:38 -0600

    Closes https://github.com/Lokathor/gba2k/issues/4

--- src/rt0.s
+++ src/rt0.s
@@ -10,6 +10,7 @@
     .space 0xE0
 
   asm_init:
+    /* We're gonna "pin" r12 to be MMIO_BASE during our initialization. */
     mov r12, #0x04000000
 
     set_waitcnt:
@@ -19,18 +20,16 @@
       strh r1, [r0]
 
     iwram_copy:
-      /* Check the number of bytes in (__iwram_end - __iwram_start), and skip
-      this part if it's zero. */
-      ldr r0, =__iwram_start
-      ldr r1, =__iwram_end
-      subs r4, r1, r0
+      /* If our iwram copy is 0 words we skip this bit. But, it's almost never
+      gonna be zero words. */
+      ldr r4, =__iwram_word_copy_count
+      cmp r4, #0
       beq 1f
-      /* Uses DMA3 to copy the required number of words from ROM to IWRAM. */
+      ldr r0, =__iwram_start
       add r3, r12, #0xD4 /* DMA3_BASE */
       ldr r2, =__iwram_position_in_rom
       str r2, [r3] /* set source */
       str r0, [r3, #4] /* set destination */
-      lsr r4, r4, #2 /* Convert bytes to words */
       strh r4, [r3, #8] /* set word count */
       mov r5, #0x8400 /* 32-bit transfers, DMA Enabled */
       strh r5, [r3, #10] /* set control bits */
@@ -39,16 +38,13 @@
       touch DMA again for at least that much time so it's fine. */
     
     bss_zeroing:
-      /* Check the number of bytes in (__bss_end - __bss_start), and skip
-      this part if it's zero. */
-      ldr r0, =__bss_start
-      ldr r1, =__bss_end
-      subs r4, r1, r0
+      /* Similarly to iwram, we skip this block if there's no words to clear. */
+      ldr r4, =__bss_word_clear_count
+      cmp r4, #0
       beq 1f
-      /* Zero out one word at a time until we're done. TODO: this should probably
-      also use DMA3 to go faster. */
+      ldr r0, =__bss_start
+      /* Zero out one word at a time. TODO: use DMA? */
       mov r2, #0
-      lsr r4, r4, #2 /* Convert bytes to words */
       .L_write_loop:
       str r2, [r0], #4
       subs r4, r4, #1
@@ -63,7 +59,7 @@
     /* The `main` function should *not* return, but we'll set the link register
     anyway so that if the programmer defines a main that returns on accident we
     can just go back to the start of the program. */
-    ldr lr, =asm_init
+    adr lr, asm_init
     ldr r0, =main
     bx r0
 
@@ -103,51 +99,72 @@
     add r12, r0, #0x208 @r12=&IME
     mov r3, #0
     swp r3, r3, [r12]   @IME swap off
+    /* Still Important
+    * r12, IME
+    * r3, ime_previous
+    */
 
     read_update_hardware_flags:
     ldr r0, [r12, #-8]      @r0=IE_IF
     and r0, r0, r0, LSR #16 @r0=IE&IF
     strh r0, [r12, #-6]     @IF=r0
+    /* Still Important
+    * r12, IME
+    * r3, ime_previous
+    * r0, irq_flags
+    */
 
     read_update_bios_flags:
     sub  r2, r12, #(0x208+8) @r2=&BIOS_IW
     ldrh r1, [r2]            @r1=BIOS_IW
     orr  r1, r1, r0          @r1=r1|r0
     strh r1, [r2]            @BIOS_IW=r0
+    /* Still Important
+    * r12, IME
+    * r3, ime_previous
+    * r0, irq_flags
+    */
 
     get_rust_fn_ptr:
     ldr r1, =RUST_IRQ_HANDLER
     ldr r1, [r1]       @r1==RUST_IRQ_HANDLER
     cmp r1, #0         @if r1==0
     beq end_of_rt0     @then branch
+    /* Still Important
+    * r12, IME
+    * r3, ime_previous
+    * r1, rust_irq_fn
+    * r0, irq_flags
+    */
 
     call_rust_fn_in_sys_mode:
     mrs r2, SPSR      @save SPSR
     push {r0, r2}     @push SPSR (SVC)
 
     mov r2, #0b00011111
-    msr CPSR_cf, r2   @SYS mode
+    msr CPSR_cf, r2   @set SYS mode
 
-    /* Pushing r2 is useless here, but we just need to push an even total number
-    of registers. We want to keep, at minimum, r3 and lr. Also, it would be nice
-    to store r12. We could just rebuild r12 after the call, but that takes about
-    as much time as just pushing two extra registers. This means we need one
-    extra register to have an even number, and so I picked r2.
+    /* We need to push an even number of registers here. We also need to save,
+    at minimum, r3 (ime_previous) and lr (return_address). We could also save
+    r12 and any junk register, but that costs +2 cycles before *and* after the
+    call, and just rebuilding the r12 value after is only 2 cycles.
     */
-    push {r2, r3, r12, lr} 
+    push {r3, lr} @push regs (SYS)
 
-    ldr lr, =1f
+    adr lr, 1f
     bx r1
     1:
 
-    pop {r2, r3, r12, lr}
+    pop {r3, lr} @pop regs (SYS)
 
     mov r2, #0b10010010
-    msr CPSR_cf, r2   @SVC mode
+    msr CPSR_cf, r2   @set SVC mode
 
     pop {r0, r2}      @pop SPSR (SVC)
     msr SPSR, r2      @restore SPSR
-    @ still important, r3, r12
+    /* Still Important
+    * r3, ime_previous
+    */
 
     end_of_rt0:
     mov r12, #0x04000000
